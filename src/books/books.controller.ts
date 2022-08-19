b3d7a3f7d5fb8250b9fb2152402570120b63d67a
Diego Pagini <diego.pagini@neoris.com>
2022-08-19 11:31:38 +0200

    PUT && DELETE

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,22 +1,34 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('books') // This is the url that is goin to be called. Like localhost:3000/books
 export class BooksController {
   @Get() // This is the method
-  getBooks() {
+  getBooks(): string {
     // This is the function that will be executed
     return 'Aquí se listarán todos los libros';
   }
 
   @Get(':id') // To use params
-  getBookById(@Param() params) {
+  getBookById(@Param() params): string {
     // To inject params
     // console.log({ params }); // Output: { params: { id: '1' } }
     return `The Book with id: ${params.id}`;
   }
 
   @Post() // The variables are passed in the body, not by params
-  createBook(@Body() book) {
+  createBook(@Body() book): string {
     return `Book created: ${JSON.stringify(book)}`; // Output: Book created: {"name":"ABC","id":"1","author":"Frank"} <= body.
+  }
+
+  @Put(':id') // In put we use params
+  editBook(@Param('id') id): string {
+    // @Param('id') this way we can desestructure params.
+    // URL example: localhost:3000/books/1
+    return `The book with id: ${id} has been updated`;
+  }
+
+  @Delete(':id')
+  deleteBook(@Param('id') id): string {
+    return `The book with id: ${id} has been deleted`;
   }
 }
