a5cee6fd6fc130d53424f8b6cde01fdd5bc2f379
Diego Pagini <diego.pagini@neoris.com>
2022-08-19 11:05:22 +0200

    POST

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('books') // This is the url that is goin to be called. Like localhost:3000/books
 export class BooksController {
@@ -13,5 +13,10 @@ export class BooksController {
     // To inject params
     // console.log({ params }); // Output: { params: { id: '1' } }
     return `The Book with id: ${params.id}`;
+  }
+
+  @Post() // The variables are passed in the body, not by params
+  createBook(@Body() book) {
+    return `Book created: ${JSON.stringify(book)}`; // Output: Book created: {"name":"ABC","id":"1","author":"Frank"} <= body.
   }
 }
