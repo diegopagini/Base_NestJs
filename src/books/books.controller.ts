d217964254c7c29644ed552556c0cfe682808ded
Diego Pagini <diego.pagini@neoris.com>
2022-08-19 10:59:24 +0200

    GET

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
-@Controller('books')
-export class BooksController {}
+@Controller('books') // This is the url that is goin to be called. Like localhost:3000/books
+export class BooksController {
+  @Get() // This is the method
+  getBooks() {
+    // This is the function that will be executed
+    return 'Aquí se listarán todos los libros';
+  }
+
+  @Get(':id') // To use params
+  getBookById(@Param() params) {
+    // To inject params
+    // console.log({ params }); // Output: { params: { id: '1' } }
+    return `The Book with id: ${params.id}`;
+  }
+}
