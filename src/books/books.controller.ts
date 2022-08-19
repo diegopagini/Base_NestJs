6a5d134d7438daee9695c8d92dc58d84f6d49c1f
Diego Pagini <diego.pagini@neoris.com>
2022-08-19 11:38:00 +0200

    DTO

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
+import { CreateBookDTO } from './dto/create-book.dto';
+
 @Controller('books') // This is the url that is goin to be called. Like localhost:3000/books
 export class BooksController {
   @Get() // This is the method
@@ -16,19 +18,19 @@ export class BooksController {
   }
 
   @Post() // The variables are passed in the body, not by params
-  createBook(@Body() book): string {
-    return `Book created: ${JSON.stringify(book)}`; // Output: Book created: {"name":"ABC","id":"1","author":"Frank"} <= body.
+  createBook(@Body() book: CreateBookDTO): string {
+    return `Book created: ${JSON.stringify(book)}`; // Output: Book created: {"name":"ABC","pages":"1","author":"Frank"} <= body.
   }
 
   @Put(':id') // In put we use params
-  editBook(@Param('id') id): string {
+  editBook(@Param('id') id: number, @Body() updateBook: CreateBookDTO): string {
     // @Param('id') this way we can desestructure params.
     // URL example: localhost:3000/books/1
     return `The book with id: ${id} has been updated`;
   }
 
   @Delete(':id')
-  deleteBook(@Param('id') id): string {
+  deleteBook(@Param('id') id: number): string {
     return `The book with id: ${id} has been deleted`;
   }
 }
---
+++ src/books/dto/create-book.dto.ts
@@ -0,0 +1,6 @@
+// DTO = Data Transfer Object
+export class CreateBookDTO {
+  name: string;
+  author: string;
+  pages: number;
+}
